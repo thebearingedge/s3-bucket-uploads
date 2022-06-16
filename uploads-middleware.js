577525c0eb9838c591c870c04eebf258d51217c2
Tim Davis <7432943+thebearingedge@users.noreply.github.com>
2022-06-16 12:02:42 -0700

    update to use @aws-sdk and multer-s3 v3

--- uploads-middleware.js
+++ uploads-middleware.js
@@ -1,31 +1,31 @@
+const fs = require('fs')
 const path = require('path')
-const mime = require('mime')
 const multer = require('multer')
 const multerS3 = require('multer-s3')
-const S3 = require('aws-sdk/clients/s3')
+const { S3 } = require('@aws-sdk/client-s3')
 
 // connect us to S3 on AWS
 const s3 = new S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  region: process.env.AWS_S3_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  }
 })
 
 // create a storage adapter
 const storage = multerS3({
   s3: s3,
+  acl: 'public-read',
   bucket: process.env.AWS_S3_BUCKET,
-  acl: 'public-read', // so that users can view the files
+  // ☝️ so that anyone with a url can view the file
+  contentType: multerS3.AUTO_CONTENT_TYPE,
+  // ☝️ so that when the file is downloaded,
+  // the proper Content-Type header is set in the response
   key: (req, file, done) => {
     const fileExtension = path.extname(file.originalname)
-    // include the extension in the key (.jpg, .png, .wav, .mp4, etc)
-    const key = `${Date.now()}${fileExtension}`
-    done(null, key)
+    done(null, `${Date.now()}${fileExtension.toLowerCase()}`)
   },
-  contentType: (req, file, done) => {
-    // tell S3 what Content-Type to use when serving the file
-    const contentType = mime.getType(file.originalname)
-    done(null, contentType)
-  }
 })
 
 // create the middleware
@@ -33,7 +33,8 @@ const uploadsMiddleware = multer({
   storage: storage,
 }).single('file-to-upload')
 
-// the string argument to .single()
-// matches the <input name="file-to-upload" type="file"/>
+// the string argument to .single() is the file's field name
+// it matches the <input name="file-to-upload" type="file"/>
+// or the key of the FormData field you appended
 
 module.exports = uploadsMiddleware
